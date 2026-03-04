7f5392040c02026810ecdc5177115402b4d4ff4e
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 15:35:58 +0530

    initial commit

---
+++ src/main/java/com/medisphere/doctor/MedisphereDoctorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereDoctorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereDoctorServiceApplication.class, args);
+	}
+
+}
