738db6c8462655bf11654c348d608b988e19e817
DasunD2002 <dasun5967@gmail.com>
2026-04-17 01:39:06 +0530

    .

--- src/main/java/com/medisphere/doctor/repository/DoctorRepository.java
+++ src/main/java/com/medisphere/doctor/repository/DoctorRepository.java
@@ -15,8 +15,7 @@ import java.util.List;
 public interface DoctorRepository extends JpaRepository<DoctorEntity, Integer> {
 
     @Query(value = "SELECT * FROM medisphere_doctor WHERE doctor_id = :#{#doctorId.doctorId}", nativeQuery = true)
-    DoctorEntity getDoctorByDoctorId(
-            @org.springframework.data.repository.query.Param("doctorId") GetByIdDoctorDTO doctorId);
+    DoctorEntity getDoctorByDoctorId(@org.springframework.data.repository.query.Param("doctorId") GetByIdDoctorDTO doctorId);
 
     DoctorEntity findByDoctorId(String doctorId);
 
